071069232c72d96112b05988d14b05914d26e24f
Thomas Li <guslithd@student.gu.se>
2019-02-20 14:53:22 +0100

    Added list/recursion example to arith.hs

--- haskell-mutate2/arith.hs
+++ haskell-mutate2/arith.hs
@@ -3,6 +3,11 @@
 add :: Integer -> Integer -> Integer
 add x y = x + y
 
+filterEven :: [Integer] -> [Integer]
+filterEven [] = []
+filterEven (x:xs) | even x    = x : filterEven xs
+                  | otherwise = filterEven xs
+
 double :: (Num a) => a -> a
 double n = n * 2
 
