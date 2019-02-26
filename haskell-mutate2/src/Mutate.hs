585ea2c7f378d71015d233384388ee6f662c9544
Thomas Li <guslithd@student.gu.se>
2019-02-26 14:22:16 +0100

    Added snippets from meeting

--- haskell-mutate2/src/Mutate.hs
+++ haskell-mutate2/src/Mutate.hs
@@ -8,9 +8,23 @@ import Language.Haskell.Exts
 -- | Defining a class, "Mutable". The function mutate takes a member of the 
 -- class as an argument, and the result is of the same type. (?)
 class Mutable a where
-    mutate :: a -> a
+    mutate :: a -> [a]
 
+-- a -> [a] eller ngn annan struktur
 -- Other suggestions for mutations in comments
+
+-- mutate3 som tar funktion/konstruktor med flera konstruerare,
+m3 :: (Mutable a, Mutable b, Mutable c) => (a -> b -> c -> d) -> a -> b -> c -> [d]
+m3 f a b c = dsa ++ dsb ++ dsc
+    where as = mutate a
+          bs = mutate b
+          cs = mutate c
+          dsa = map (\x -> f x b c) as
+          dsb = map (\x -> f a x c) bs
+          dsc = map (\x -> f a b x) cs
+
+instance Mutable Exp a where
+    mutate (InfixApp l e1 op e2) = m3 (InfixApp l) e1 op e2
 
 instance Mutable (Name a) where
     mutate (Symbol l str) = case str of
