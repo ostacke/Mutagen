d032255c348b9ebe046643d17bdf0c9b87304ce7
Therese Andersson <gusandthak@student.gu.se>
2019-03-06 13:10:49 +0100

    Attempt at combine/robert

--- haskell-mutate2/src/Mutate.hs
+++ haskell-mutate2/src/Mutate.hs
@@ -5,6 +5,7 @@ module Mutate
 import GHC.Float
 import Language.Haskell.Exts
 import Debug.Trace
+import Data.List
 
 -- | Defining a class Mutable. The function mutate takes a member of the 
 -- class as an argument, and the result is of the same type. (?)
@@ -48,7 +49,10 @@ m4 f a b c d = aMutants ++ bMutants ++ cMutants ++ dMutants
           aMutants = map (\x -> f x b c d) aMutant
           bMutants = map (\x -> f a x c d) bMutant
           cMutants = map (\x -> f a b x d) cMutant
-          dMutants = map (\x -> f a b c x) dMutant
+          dMutants = map (\x -> f a b c x) (combine d dMutant)
+
+combine :: (Mutable a) => a -> [a] -> [a]
+combine d dmut = intersperse d dmut 
 
 -- | Same as m2, buth with five parameters
 m5 :: (Mutable a, Mutable b, Mutable c, Mutable d, Mutable e) =>
