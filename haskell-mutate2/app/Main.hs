0d391860741abb747c147955b3e20731439995d2
Thomas Li <guslithd@student.gu.se>
2019-04-03 22:51:30 +0200

    removed unused functions and imports

--- haskell-mutate2/app/Main.hs
+++ haskell-mutate2/app/Main.hs
@@ -1,23 +1,16 @@
 module Main where
 
-import Control.Exception
-import Control.Monad
 import Data.Maybe (fromJust)
 import Language.Haskell.Exts
 import System.Environment
-import System.Exit
 import System.Directory
 import System.FilePath ((</>), (-<.>), takeFileName, takeExtension)
 import System.Process
-import System.IO
-import System.IO.Error
-import Text.Pretty.Simple
 
 import FileOp
 import Results
 import Mutate
 import Path
-
 
 outputSuffix = "mutants-out"
 backupSuffix = "backups"
@@ -228,41 +221,6 @@ handleTestResult res mutant mutantPath outDir = case res of
         putStrLn ""
 
 
--- | Tries to run the mutation process on all files at the input directory, 
---   then outputting the result. Fails if the input directory does not exist.
-launchAtDir :: FilePath -> FilePath -> IO ()
-launchAtDir inputDir outputDir = do
-    absInputDir <- canonicalizePath inputDir
-    absOutputDir <- canonicalizePath outputDir
-
-    exists <- doesDirectoryExist absInputDir
-
-    if exists
-        then do
-            -- Performs the actual mutating and outputs to files
-            filePaths <- getAbsoluteDirContents inputDir
-            let hsPaths = filter (\x -> takeExtension x == ".hs") filePaths
-            mutants <- mapM mutateFile hsPaths
-
-            let combinations = combineMutants mutants
-            outputMutants absOutputDir combinations
-
-            -- Prints some information of what happened:
-            putStrLn ""
-            putStrLn "File paths of input files: "
-            mapM_ putStrLn hsPaths
-            putStrLn ""
-
-            putStrLn $ "Input directory: " ++ show absInputDir
-            putStrLn $ "Output directory: " ++ show absOutputDir
-            putStrLn ""
-
-            putStrLn $ "Total number of mutants: " ++ show (length (concat mutants))
-
-        else 
-            putStrLn $ "ERROR: Directory '" ++ inputDir ++ "' does not exist."
-
-
 -- | Attemps to parse a module at the given file path, returning the 
 --   mutation results if successful. Otherwise prints the error message.
 mutateFile :: FilePath -> IO [Module SrcSpanInfo]
@@ -283,82 +241,4 @@ mutateFile path = do
                     putStrLn errMsg
                     putStrLn ""
                     return []
-
-    
--- | Invokes combineMutants' with intended first parameter.
-combineMutants :: [[Module SrcSpanInfo]] -> [[[Module SrcSpanInfo]]]
-combineMutants = combineMutants' []
-
-
--- | Given a list of mutations lists (from `map mutate xs`), returns a 
---   list of groups created from the combinations of the different modules, 
---   where:
---   * For each group of modules, exactly one module is mutated and contains 
---     exactly one mutation, while the rest of the modules are unchanged from 
---     their original versions.
---   * Each group of modules contains all the given modules. In other words, 
---     no group will be missing a module.
-combineMutants' :: [[Module SrcSpanInfo]]
-                -> [[Module SrcSpanInfo]]
-                -> [[[Module SrcSpanInfo]]]
-combineMutants' _ [] = []
-combineMutants' xs (y:ys) = 
-    [ x : originals | x <- y ] : combineMutants' (xs ++ [y]) ys
-        where originals = getOriginals xs ++ getOriginals ys
-    -- Creates lists from all the mutations of one module. For each mutated 
-    -- module, the original versions of the other modules are also in the list. 
-
-
--- | Given a list of mutation lists (from running `map mutate xs`), returns 
---   the first element of each list. Basically a glorified `map head xs`.
-getOriginals :: [[Module SrcSpanInfo]] -> [Module SrcSpanInfo]
-getOriginals [] = []
-getOriginals (xs:xss) | null xs   = getOriginals xss
-                      | otherwise = head xs : getOriginals xss
-
-
--- | Recursively creates a tree-like folder structure at the input directory
---   and writes the mutated files.
-outputMutants :: FilePath -> [[[Module SrcSpanInfo]]] -> IO ()
-outputMutants _ [] = return ()
-outputMutants dir (x:xs) = do
-    let workingDir = dir </> show (length (x:xs))
-    createDirectoryIfMissing True workingDir
-    outputMutants' workingDir x
-
-    outputMutants dir xs
-
-
--- | In the input files for this function, all the mutations are of the same 
---   file, with the other files always being unchanged from their originals.
-outputMutants' :: FilePath -> [[Module SrcSpanInfo]] -> IO ()
-outputMutants' _ [] = return ()
-outputMutants' dir (x:xs) = do
-    let workingDir = dir </> show (length (x:xs))
-    createDirectoryIfMissing True workingDir
-    outputMutants'' workingDir x
-
-    outputMutants' dir xs
-
-
--- | Each list of inputs to this function represents a group of files where 
---   exactly one of the files contains exactly one mutation.
-outputMutants'' :: FilePath -> [Module SrcSpanInfo] -> IO ()
-outputMutants'' _ [] = return ()
-outputMutants'' dir (x:xs) = do
-    let newPath = dir </> (takeFileName $ fromJust (getModulePath x))
-
-    handle <- openFile newPath WriteMode
-    hPutStrLn handle (prettyPrint x)
-    hClose handle
-    
-    outputMutants'' dir xs
-
-
--- | Returns the absolue path of a Module, as specified in the SrcSpanInfo
-getModulePath :: Module SrcSpanInfo -> Maybe FilePath
-getModulePath mod = case mod of
-    Module (SrcSpanInfo (SrcSpan fileName _ _ _ _) _) _ _ _ _ -> Just fileName
-    _ -> Nothing
-
 
