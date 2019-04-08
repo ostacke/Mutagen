30fef82920d61fd063991400deab4dda4d5d5979
Thomas Li <guslithd@student.gu.se>
2019-04-08 10:41:09 +0200

    added seed parameters for mutateInj

--- haskell-mutate2/src/MutateInject.hs
+++ haskell-mutate2/src/MutateInject.hs
@@ -2,25 +2,27 @@ module MutateInject where
 
 import Data.List
 
+-- | mutateInj takes a "seed" and returns a pseudo-random mutation of the 
+--   input parameter.
 class Injectable a where
-    mutateInj :: a -> a
+    mutateInj :: Int -> a -> a
 
 -- TODO: Specific behaviour depending on type of a?
 instance Injectable [a] where
-    mutateInj xs = pickOne mutants
+    mutateInj n xs = pickOne n mutants
         where mutants = [reverse xs, tail xs, init xs]
 
 instance Injectable Int where
-    mutateInj n = pickOne mutants
-        where mutants = [n + 1, n - 1, -n, 0, 1, maxBound, minBound,
-                         n + maxBound, n - maxBound]
+    mutateInj n int = pickOne n mutants
+        where mutants = [int + 1, int - 1, -int, 0, 1, maxBound, minBound,
+                         int + maxBound, int - maxBound]
 
 instance Injectable Integer where
-    mutateInj n = pickOne mutants
-        where mutants = [n + 1, n - 1, -n, 0, 1, (fromIntegral n) :: Integer]
+    mutateInj n int = pickOne n mutants
+        where mutants = [int + 1, int - 1, -int, 0, 1, (fromIntegral int) :: Integer]
 
 instance Injectable Double where
-    mutateInj r = pickOne mutants
+    mutateInj n r = pickOne n mutants
         where mutants = [0, 1, r+1, r-1, r*(-1), r/2, r*2, r/10, r*10,
                         (r * 2)/2, (r/2)*2, 
                         fromIntegral $ truncate r,
@@ -29,7 +31,7 @@ instance Injectable Double where
                         fromIntegral $ round r]
 
 instance Injectable Float where
-    mutateInj r = pickOne mutants
+    mutateInj n r = pickOne n mutants
         where mutants = [0, 1, r+1, r-1, r*(-1), r/2, r*2, r/10, r*10,
                         (r * 2)/2, (r/2)*2, 
                         fromIntegral $ truncate r,
@@ -38,7 +40,7 @@ instance Injectable Float where
                         fromIntegral $ round r]
 
 instance Injectable Char where
-    mutateInj c = pickOne mutants
+    mutateInj n c = pickOne n mutants
         where mutants = constChars ++ charMuts c
               constChars = ['x', 'a', '%', '\0', '\n']
               charMuts c = filter (/= c) $ case c of
@@ -47,7 +49,7 @@ instance Injectable Char where
                                             _ -> [succ c, pred c]
 
 instance Injectable (Maybe a) where
-    mutateInj m = pickOne mutants
+    mutateInj n m = pickOne n mutants
         where mutants = case m of
                             Just a -> [Nothing]
         
@@ -59,9 +61,8 @@ instance Injectable (a, a) where
 -}                  
 
 -- TODO
-pickOne :: [a] -> a
-pickOne (x:xs) = x
-pickOne _      = error "pickOne on empty list"
+pickOne :: Int -> [a] -> a
+pickOne n xs = xs !! ((9999 * n) `mod` length xs)
 
 
 
