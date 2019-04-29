d7bcb3733b0e7cd5347adfb3f950a4b6085aefd2
Thomas Li <guslithd@student.gu.se>
2019-04-29 16:42:13 +0200

    Added duplicate filtering to Literal types

--- haskell-mutate2/src/MutateInject.hs
+++ haskell-mutate2/src/MutateInject.hs
@@ -9,49 +9,53 @@ class Injectable a where
 
 -- TODO: Specific behaviour depending on type of a?
 instance Injectable [a] where
-    mutateInj n xs = pickOne n mutants
+    mutateInj _ []     = []
+    mutateInj _ (_:[]) = []
+    mutateInj n xs     = pickOne n mutants
         where mutants = [reverse xs, tail xs, init xs]
 
 instance Injectable Int where
     mutateInj n int = pickOne n mutants
-        where mutants = [int + 1, int - 1, -int, 0, 1, maxBound, minBound,
-                         int + maxBound, int - maxBound]
+        where mutants = filter (/= int) 
+                [int + 1, int - 1, -int, 0, 1, maxBound, minBound,
+                 int + maxBound, int - maxBound]
 
 instance Injectable Integer where
     mutateInj n int = pickOne n mutants
-        where mutants = [int + 1, int - 1, -int, 0, 1, (fromIntegral int) :: Integer]
+        where mutants = filter (/= int)
+                [int + 1, int - 1, -int, 0, 1, (fromIntegral int) :: Integer]
 
 instance Injectable Double where
     mutateInj n r = pickOne n mutants
-        where mutants = [0, 1, r+1, r-1, r*(-1), r/2, r*2, r/10, r*10,
-                        (r * 2)/2, (r/2)*2, 
-                        fromIntegral $ truncate r,
-                        fromIntegral $ floor r, 
-                        fromIntegral $ ceiling r, 
-                        fromIntegral $ round r]
+        where mutants = filter (/= r) 
+                [0, 1, r+1, r-1, r*(-1), r/2, r*2, r/10, r*10,
+                 (r * 2)/2, (r/2)*2, 
+                 fromIntegral $ truncate r,
+                 fromIntegral $ floor r, 
+                 fromIntegral $ ceiling r, 
+                 fromIntegral $ round r]
 
 instance Injectable Float where
     mutateInj n r = pickOne n mutants
-        where mutants = [0, 1, r+1, r-1, r*(-1), r/2, r*2, r/10, r*10,
-                        (r * 2)/2, (r/2)*2, 
-                        fromIntegral $ truncate r,
-                        fromIntegral $ floor r, 
-                        fromIntegral $ ceiling r, 
-                        fromIntegral $ round r]
+        where mutants = filter (/= r)
+                [0, 1, r+1, r-1, r*(-1), r/2, r*2, r/10, r*10,
+                 (r * 2)/2, (r/2)*2, 
+                 fromIntegral $ truncate r,
+                 fromIntegral $ floor r, 
+                 fromIntegral $ ceiling r, 
+                 fromIntegral $ round r]
 
 instance Injectable Char where
     mutateInj n c = pickOne n mutants
-        where mutants = constChars ++ charMuts c
+        where mutants = filter (/= c) $ constChars ++ charMuts c
               constChars = ['x', 'a', '%', '\0', '\n']
-              charMuts c = filter (/= c) $ case c of
-                                            '\NUL'     -> [succ c]
-                                            '\1114111' -> [pred c]
-                                            _ -> [succ c, pred c]
+              charMuts x = case x of
+                                '\NUL'     -> [succ c]
+                                '\1114111' -> [pred c]
+                                _ -> [succ c, pred c]
 
 instance Injectable (Maybe a) where
-    mutateInj n m = pickOne n mutants
-        where mutants = case m of
-                            Just a -> [Nothing]
+    mutateInj _ _ = Nothing
         
 pickOne :: Int -> [a] -> a
 pickOne n xs = xs !! ((9999 * n) `mod` length xs)
